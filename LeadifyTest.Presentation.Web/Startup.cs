ce561d9d8929a1cb8989b23a13ce4b25d00a8c6c
StuartBacklund <stuart.backlund@gmail.com>
2017-03-01 16:18:25 +0200

    Solution Setup 1-Added basic mvc project 2-Created Database plus table

---
+++ LeadifyTest.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeadifyTest.Presentation.Web.Startup))]
+namespace LeadifyTest.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
